09b148d1e250582f9a57036bcb05391def6f1909
VicRom5000 <victor45132@gmail.com>
2024-02-10 19:00:36 -0400

    cambio del Schema Auto

--- models/Auto.js
+++ models/Auto.js
@@ -1,13 +1,14 @@
-const Car = require('mongoose');
+const mongoose = require('mongoose');
 
-var CarSchema = Car.Schema({
+var AutoSchema = mongoose.Schema({
 
   modelo                    : String, 
   placa                     : String,
   licencia                  : String, 
-  usuario                   : String, 
+  usuario                   : { type: Schema.Types.ObjectId, ref: 'User' },
   date_created              : { type: Date, default: Date.now }, 
   date_updated              : { type: Date, default: Date.now },
 });
 
+module.exports = mongoose.model( 'Auto', mangasSchema );
 
